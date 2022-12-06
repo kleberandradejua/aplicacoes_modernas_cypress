092373c2efc31b0fe6be4f6fb08420ce6bdbc985
kleberandradejua <kleberandrade98@gmail.com>
2022-12-06 19:54:49 -0300

    Finalizando aprendizagem básica com Cypress

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,5 +1,30 @@
 /// <reference types="cypress" />
 
 describe('Cypress basics', () => {
+    it('Deve visitar a página e verificar o título', () =>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.title()
+        .should('be.equal', 'Campo de Treinamento')
+        .and('contain', 'Campo')
+
+        const title = cy.title()
+        console.log(title)
+
+        cy.get('#formNome').type(title)
+
+    })
+
+    it.only('Encontrar e interagir com um elemento', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.pause()
+
+        cy.get('input[id="buttonSimple"]')
+        .should('have.value', 'Clique Me!')
+        .click()
+        .should('have.value', 'Obrigado!')
+
+    })
     
 })
